d377910583aeaf9752ac5a13eb9657c2472615b5
Student <Student@10.7.10.83>
2025-11-18 09:53:25 +0100

    commit DamyanDatabase

---
+++ DamyanDatabase/src/main/java/wat/jeet/damyandatabase/DamyanDatabase.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package wat.jeet.damyandatabase;
+
+/**
+ *
+ * @author Student
+ */
+public class DamyanDatabase {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
